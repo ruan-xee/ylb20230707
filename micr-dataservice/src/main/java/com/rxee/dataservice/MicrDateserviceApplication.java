a422640e0e69ab3ea9e552daed35017209335f70
ruanzh <ruanzh@yinhai.com>
2023-07-10 16:16:05 +0800

    <chore>:对pom依赖进行搭建重构，对application做基本配置

--- micr-dataservice/src/main/java/com/rxee/dataservice/MicrDateserviceApplication.java
+++ micr-dataservice/src/main/java/com/rxee/dataservice/MicrDateserviceApplication.java
@@ -1,9 +1,13 @@
 package com.rxee.dataservice;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableDubbo
 @SpringBootApplication
+@MapperScan(basePackages = "com.rxee.dataservice.mapper")
 public class MicrDateserviceApplication {
 
     public static void main(String[] args) {
